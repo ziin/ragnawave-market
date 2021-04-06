622a07952702d22ab130d58cdcc1d8b1b4c3bb7a
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2021-04-06 08:49:31 -0400

    feat: memoize item image

--- src/components/ItemImage.tsx
+++ src/components/ItemImage.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@chakra-ui/layout";
 import Image from "next/image";
+import { useMemo } from "react";
 
 interface Props {
   id: number;
@@ -9,17 +10,20 @@ interface Props {
 }
 
 export default function ItemImage({ id, alt, size = 24, isCard }: Props) {
-  return (
-    <Box mr="1">
-      <Image
-        src={`http://www.ragnawave.com.br/dist/db/items/${
-          isCard ? 4297 : id
-        }.png`}
-        layout="fixed"
-        width={size}
-        height={size}
-        alt={alt}
-      />
-    </Box>
+  return useMemo(
+    () => (
+      <Box mr="1">
+        <Image
+          src={`http://www.ragnawave.com.br/dist/db/items/${
+            isCard ? 4297 : id
+          }.png`}
+          layout="fixed"
+          width={size}
+          height={size}
+          alt={alt}
+        />
+      </Box>
+    ),
+    [id, alt, size, isCard]
   );
 }
